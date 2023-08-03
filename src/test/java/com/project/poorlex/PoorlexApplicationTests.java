fa62e7f9d8678a5d26803eb8e362d14710e7f215
ikjo39 <chi4321@naver.com>
2023-08-03 13:35:42 +0900

    build: 프로젝트 초기 설정

---
+++ src/test/java/com/project/poorlex/PoorlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poorlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoorlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
