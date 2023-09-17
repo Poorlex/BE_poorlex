38c1128d9635805ce7f38c9937a6a5f7c7e30309
chenj <chenj7498@gmail.com>
2023-09-17 18:43:26 +0900

    커밋 변경이 없이 에러 뜬 부분 재커밋

--- src/main/java/com/project/poorlex/domain/goal/GoalRepository.java
+++ src/main/java/com/project/poorlex/domain/goal/GoalRepository.java
@@ -2,5 +2,10 @@ package com.project.poorlex.domain.goal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GoalRepository extends JpaRepository<Goal, Long> {
+    List<Goal> findAllByMemberId_OauthIdAndIsCompletedFalse(String oauthId);
+
+    List<Goal> findAllByMemberId_OauthIdAndIsCompletedTrue(String oauthId);
 }
